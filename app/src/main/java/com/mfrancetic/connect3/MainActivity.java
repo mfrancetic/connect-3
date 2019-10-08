9d67a7099ade004d3fa2a89e7b38e4819ba3706b
Maja Francetic <francetic.maja@gmail.com>
2019-10-08 16:09:10 +0200

    Add TextView, Button and GridView with 9 items to the activity_main

--- app/src/main/java/com/mfrancetic/connect3/MainActivity.java
+++ app/src/main/java/com/mfrancetic/connect3/MainActivity.java
@@ -3,6 +3,7 @@ package com.mfrancetic.connect3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.GridLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GridLayout gridLayout = findViewById(R.id.grid_layout);
     }
 }
