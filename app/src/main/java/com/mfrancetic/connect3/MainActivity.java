b7324d4a00fc1dba3e8d6b964e0d73c483cbf311
Maja Francetic <francetic.maja@gmail.com>
2019-10-08 18:53:54 +0200

    Add setImage method as the onClick to the image Views and hide the play again button and message TextView

--- app/src/main/java/com/mfrancetic/connect3/MainActivity.java
+++ app/src/main/java/com/mfrancetic/connect3/MainActivity.java
@@ -3,15 +3,37 @@ package com.mfrancetic.connect3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.GridLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button playAgainButton;
+
+    private TextView messageTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        GridLayout gridLayout = findViewById(R.id.grid_layout);
+        playAgainButton = findViewById(R.id.play_again_button);
+        messageTextView = findViewById(R.id.information_text_view);
+
+        playAgainButton.setVisibility(View.INVISIBLE);
+        messageTextView.setVisibility(View.INVISIBLE);
+    }
+
+    public void setImage(View view) {
+        int id = view.getId();
+        ImageView imageView = findViewById(id);
+        imageView.setImageResource(R.drawable.banana);
     }
 }
